27267c3a15ec638c8c31872227f48efb1d721db3
dopieralamikolaj <dopierala.mikolaj@gmail.com>
2017-11-13 19:24:04 +0100

    Laboratorium - Suma kontrolna

---
+++ sumakontrolna.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PSlab1
+{
+    class HelloWorld 
+    {
+        static void Main()
+        {
+            string zmienna = "tekst";
+            int m, n, v;
+            int suma = 0;
+            
+            for (int i = 0; i < zmienna.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    m = (1<<j);
+                    v = zmienna[i];
+                    n = (v & m) >> j;
+
+                    suma ^= n;
+                    
+                }
+            }
+        }
+    }
+}
